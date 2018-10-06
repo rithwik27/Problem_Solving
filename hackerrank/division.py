42b99be12dff7faf37dad2f2597e7db1d942bba3
rsani1234 <31400872+rsani1234@users.noreply.github.com>
2018-10-06 22:18:50 +0530

    Update division.py

--- hackerrank/division.py
+++ hackerrank/division.py
@@ -1,6 +1,5 @@
 # Task
 # Read two integers and print two lines. The first line should contain integer division, // . The second line should contain float division, / .
-// good codce
 # You don't need to perform any rounding or formatting operations.
 
 # Input Format
